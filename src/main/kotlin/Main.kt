cf4529e6f9f2f6c714dcbfb2bcc02623f386faec
symptoms-vader <symptomsvader@gmail.com>
2023-08-08 09:30:24 +0300

    kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    //variables
+    var name = "Mumbii"
+    println(name)
+    var age = " 36"
+    println(age)
+    var city:String = "Nakuru"
+    println(city)
+}
---
+++ src/main/kotlin/varfile.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var num1 = 84
+    var num2 = 45
+
+    var result = num1 + num2
+    println(result)
+
+    var result2 = num1 * num2
+    println(result2)
+
+    var result3 = num1 / num2
+    println(result3)
+
+    var result4 = num1 - num2
+    println(result4)
+
+}
