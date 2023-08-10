61128c304a37f2b6278e13837aa4dbb56eaaa124
symptoms-vader <symptomsvader@gmail.com>
2023-08-10 10:05:36 +0300

    calculator

--- src/main/kotlin/simpleEX.kt
+++ src/main/kotlin/simpleEX.kt
@@ -1,7 +1,9 @@
+import java.util.Calendar
+
 fun main(args: Array<String>) {
     println("Enter your birth year ")
     var birth_year:Int = readln().toInt()
-    var year = 2023
+    var year = Calendar.getInstance().get(Calendar.YEAR)
     var result = year - birth_year
 
     println("You are $result years old")
